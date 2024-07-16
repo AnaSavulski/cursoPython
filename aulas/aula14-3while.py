4513c264da16e77f08ab7caf1ede72b1b98bb638
AnaSavulski <108679110+AnaSavulski@users.noreply.github.com>
2024-07-16 20:56:08 -0300

    aula 14

--- aulas/aula14-3while.py
+++ aulas/aula14-3while.py
@@ -1,1 +1,6 @@
 # enquanto não conseguir determinanda codição, continue o laço.
+numero = -1
+while numero <= 0:
+    numero = int(input("Digite um número positivo: "))
+
+print("Você digitou:", numero)
